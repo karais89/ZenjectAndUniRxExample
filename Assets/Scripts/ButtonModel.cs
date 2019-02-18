a91f62b08afcc85747fbf031aec0f9ec74271681
karais89 <karais89@gmail.com>
2019-02-19 08:36:42 +0900

    https://techblog.gracetory.co.jp/entry/2017/08/04/164429 관련 샘플 작성

---
+++ Assets/Scripts/ButtonModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ButtonModel
+{
+    public ReactiveProperty<int> PushCounter { get; private set; }
+
+    public ButtonModel()
+    {
+        //PushCounter = new ReactiveProperty<int>(); // 초기화
+        //PushCounter.Value = 0;
+        PushCounter = new ReactiveProperty<int>(0); // 초기화
+    }
+
+    // 카운터 처리
+    public void PushCount()
+    {
+        this.PushCounter.Value++; // 여기서 이벤트가 통지된다
+    }
+}
---
+++ Assets/Scripts/ButtonPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPresenter : MonoBehaviour
+{
+    // view (ui)
+    public Button button;
+    public Text counterText;
+
+    // model
+    private ButtonModel buttonModel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 모델 새로 작성
+        this.buttonModel = new ButtonModel();
+     
+        // 누를때마다 카운터 하는 처리 등록
+        this.button.OnClickAsObservable() // 버튼을 누를때
+            .Subscribe(_ => buttonModel.PushCount()); // 카운터 증가를 등록
+        
+        // 카운터를 텍스트에 반영
+        buttonModel.PushCounter // 카운터가 변경 되었을때
+            .Subscribe(countNum => this.counterText.text = countNum.ToString()); // 텍스트 ui 변경
+    }
+}
